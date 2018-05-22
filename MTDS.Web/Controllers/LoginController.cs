b1589e3fcde4171802d11abe73cfbf551f94c79a
huang0007 <39235266+huang0007@users.noreply.github.com>
2018-05-22 22:10:40 +0800

    login
    登陆

--- MTDS.Web/Controllers/LoginController.cs
+++ MTDS.Web/Controllers/LoginController.cs
@@ -7,17 +7,20 @@ using MTDS.BLL;
 using MTDS.Common;
 using MTDS.Model;
 
+
 namespace MTDS.Web.Controllers
 {
     public class LoginController : Controller
     {
         private UserBll uBll=new UserBll();
 
+        private const int pageSize = 10;
 
         public ActionResult Index()
         {
             return View();
         }
+
         /// <summary>
         /// 登录
         /// </summary>
@@ -50,12 +53,11 @@ namespace MTDS.Web.Controllers
                         cookie.Values.Add("password", password.Trim());
                         cookie.Expires = System.DateTime.Now.AddDays(7.0);
                         HttpContext.Response.Cookies.Add(cookie);
+                        Session["userId"] = model.UserID;
+                        Session["userName"] = model.LoginName;
+                        Session["realName"] = model.Username;
+                        resultMsg = "success";
                     }
-
-                    Session["userId"] = model.UserID;
-                    Session["userName"] = model.LoginName;
-                    Session["realName"] = model.Username;
-                    resultMsg = "success";
                 }
             }
             else
@@ -65,41 +67,6 @@ namespace MTDS.Web.Controllers
 
             return Json(resultMsg, JsonRequestBehavior.AllowGet);
         }
-        /// <summary>
-        /// 注册
-        /// </summary>
-        /// <returns></returns>
-        [HttpPost]
-        public JsonResult Register()
-        {
-            var resultMsg = "";
-            var userName = HttpContext.Request.Form["txtUserName"];
-            var realName = Request.Form["realName"];
-            var password = Request.Form["password"];
-            var mobile = Request.Form["mobile"];
-            var telephone = Request.Form["telephone"];
-            var email = Request.Form["email"];
-            var address = Request.Form["address"];
-            var provinceId = Request.Form["area1"];
-            var cityId = Request.Form["area2"];
-            var countryId = Request.Form["area3"];
-
-            var model = uBll.GetByAccount(userName);
-            if (model != null)
-            {
-                resultMsg = "当前用户已存在!";
-            }
-            else
-            {
-                var pp = new Users()
-                {
-                    UserID = Guid.NewGuid(),
-
-                };
-            }
-
-
-            return null;
-        }
+      
     }
 }
--- MTDS.Web/Controllers/UserController.cs
+++ MTDS.Web/Controllers/UserController.cs
@@ -3,18 +3,101 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MTDS.BLL;
+using MTDS.Common;
+using MTDS.Model;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 
 namespace MTDS.Web.Controllers
 {
     public class UserController : Controller
     {
-        //
-        // GET: /User/
-
+        private UserBll uBll=new UserBll();
+        private const int pageSize = 10;
         public ActionResult Index()
         {
             return View();
         }
 
+        public string GetList()
+        {
+            var pageIndex = Convert.ToInt32(Request.Form["pageIndex"]);
+            var list = uBll.GetList();
+
+            if (list.Count() >= 0)
+            {
+                int pageCount = (list.Count() - 1) / pageSize + 1; //总页数
+                int totalCount = list.Count; //总记录数
+
+                list = list.Skip((pageIndex - 1) * pageSize)
+                    .Take(pageSize)
+                    .ToList();
+                string navStr = Tools.ShowPageNavigate(pageSize, pageIndex, totalCount);
+
+                var dataT = new { T = list, NavPageStr = navStr, Total = totalCount };
+                IsoDateTimeConverter timeFormat = new IsoDateTimeConverter();
+                timeFormat.DateTimeFormat = "yyyy-MM-dd";
+
+               return JsonConvert.SerializeObject(dataT, Formatting.Indented, timeFormat);
+            }
+            else
+            {
+              return  JsonConvert.SerializeObject("error");
+            }
+
+        }
+
+        /// <summary>
+        /// 添加
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        public JsonResult Register()
+        {
+            var resultMsg = "";
+            var userName = HttpContext.Request.Form["txtRealName"];
+            var loginName = Request.Form["txtloginName"];
+            var password = Request.Form["txtPassword"];
+            var mobile = Request.Form["txtTel"];
+            var email = Request.Form["txtEmail"];
+            var address = Request.Form["txtAddress"];
+            var provinceId = Request.Form["area1"];
+            var cityId = Request.Form["area2"];
+            var countryId = Request.Form["area3"];
+
+            var model = uBll.GetByAccount(userName);
+            if (model != null)
+            {
+                resultMsg = "当前用户已存在!";
+            }
+            else
+            {
+                var pp = new Users()
+                {
+                    UserID = Guid.NewGuid(),
+                    Username = "系统管理员",
+                    LoginName = "Admin",
+                    Password = Md5Encoding.Encoding("123456"),
+                    Address = "山西省太原市小店区",
+                    Lastlogintime = DateTime.Now,
+                    CreateTime = DateTime.Now,
+                    ModifyTime = DateTime.Now
+                };
+
+
+                var rst = uBll.Insert(pp);
+                if (rst > 0)
+                {
+                    resultMsg = "success";
+                }
+                else
+                {
+                    resultMsg = "error";
+                }
+            }
+
+            return Json(resultMsg, JsonRequestBehavior.AllowGet);
+        }
     }
 }
