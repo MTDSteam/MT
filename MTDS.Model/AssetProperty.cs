b84363f538a70da51c1c46a7632777b9472e92ca
huang0007 <39235266+huang0007@users.noreply.github.com>
2018-05-23 23:27:25 +0800

    1
    1

---
+++ MTDS.Model/AssetProperty.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MTDS.Model
+{
+   public class AssetProperty
+    {
+       public Guid Id { get; set; }
+       public string Name { get; set; }
+       public Guid DictionaryId { get; set; }
+       public Guid AssetTypeId { get; set; }
+       public DateTime CreateTime { get; set; }
+       public string CreateBy { get; set; }
+       public DateTime ModifyTime { get; set; }
+       public string ModifyBy { get; set; }
+    }
+}
