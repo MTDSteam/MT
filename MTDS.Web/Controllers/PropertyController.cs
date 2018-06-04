2bcc9997a80ef3bbf00e2f949e2b7a8d06c1c04e
nili8998477   email = nili8998477@126.com <39129595+nili8998477@users.noreply.github.com>
2018-06-04 19:08:38 +0800

    datatable for asset

--- MTDS.Web/Controllers/PropertyController.cs
+++ MTDS.Web/Controllers/PropertyController.cs
@@ -23,15 +23,29 @@ namespace MTDS.Web.Controllers
   
         public string getPropertyList()
         {
+          
+
+            var pageIndex = Request["page"] != null ? int.Parse(Request["page"]) : 1;
+            var pageSize= Request["rows"] != null ? int.Parse(Request["rows"]) : 10;
+            int totalCount=1;
+
             var assetTypeID = Request["assetTypeID"];
             if (!string.IsNullOrEmpty(assetTypeID))
             {
                 DataTable dt = bll.GetAssetbyType(new Guid(assetTypeID));
-                return JsonConvert.SerializeObject(dt);
+                //return JsonConvert.SerializeObject(dt);
+                var totalPages = 1;
+                var jsonData = new
+                {
+                    recordsTotal = totalCount,
+                    draw = pageIndex,
+                    recordsFiltered = totalPages,
+                    aaData = dt
+                };
+                return JsonConvert.SerializeObject(jsonData);
             }
             else
                 return null;
-      
         }
 
 
