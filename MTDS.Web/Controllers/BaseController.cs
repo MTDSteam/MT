6f39838e253b5c19a21b35bd7b75e8d95207ff53
huang0007 <39235266+huang0007@users.noreply.github.com>
2018-06-09 21:16:07 +0800

    1
    1

---
+++ MTDS.Web/Controllers/BaseController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MTDS.Web.Controllers
+{
+    public class BaseController : Controller
+    {
+        protected override void Initialize(System.Web.Routing.RequestContext requestContext)
+        {
+            base.Initialize(requestContext);
+            //下面实现用户信息相关验证的代码
+            if (requestContext.HttpContext.Session["userId"] == null)
+            {
+                requestContext.HttpContext.Response.Redirect("/Login/Index");
+            }
+        }
+
+    }
+}
